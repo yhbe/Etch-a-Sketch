ea1500b4e769df6d44eec98559a2f2d309aa7ace
SHEF <Shefqetmush@gmail.com>
2022-08-19 17:36:54 -0400

    create divs and create grid

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let div = document.createElement("div");
+var elemDiv = document.createElement("div");
+let container = document.querySelector("#container");
+container.style.display = "grid";
+
+for (let i = 0; i < 256; i++) {
+  var elemDiv = document.createElement("div");
+  elemDiv.id = "singleDiv";
+  container.appendChild(elemDiv);
+  elemDiv.innerHTML = "hi";
+}
+
+let singleDiv = document.querySelectorAll("#singleDiv");
+
+container.style.gridTemplateRows = "repeat(16, 1fr)";
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
