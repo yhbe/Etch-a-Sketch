914c74461c7664421c12c45ba4633ae8e245785e
SHEF <Shefqetmush@gmail.com>
2022-08-19 17:53:09 -0400

    added border to grid

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ for (let i = 0; i < 256; i++) {
   var elemDiv = document.createElement("div");
   elemDiv.id = "singleDiv";
   container.appendChild(elemDiv);
-  elemDiv.innerHTML = "hi";
+  elemDiv.innerHTML = "Hi";
+  elemDiv.style.borderStyle = "solid";
 }
-
-let singleDiv = document.querySelectorAll("#singleDiv");
 
 container.style.gridTemplateRows = "repeat(16, 1fr)";
 container.style.gridTemplateColumns = "repeat(16, 1fr)";
